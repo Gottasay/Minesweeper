79457b71f29e6b54b7b7335ebecbbdda85d4591c
Максим Назаров <max.mazarov2004@icloud.com>
2026-07-25 15:49:38 +0300

    main cleaning

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ from settings import Settings as s
 import pygame as pg
 from draw_schemas import draw_text_button, is_button_pressed, draw_picture_button
 
+clue_cords = [s.window[0] // 12, s.window[1] * 1.5]
+bomb_cords = (clue_cords[0], s.height // 2.25)
+flag_cords = (clue_cords[0], bomb_cords[1] + s.big_font_size)
+cur_record_cords = [s.window[0] * 1.20, s.window[1] // 4]
+best_record_cords = [s.window[0] + s.window[2] - cur_record_cords[0], cur_record_cords[1]]
+flag_amount_cords = [flag_cords[0] + s.big_font_size, flag_cords[1]]
 
-WIDTH = s.width
-HEIGHT = s.height
-LINE_COLOR = s.colors['line']
-FRAME_COLOR = s.colors['frame']
-SCREEN_COLOR = s.colors['screen']
-LINE_WIDTH = s.line_width
+button_size = s.window[2] // 8
+stop_button_cords = [s.width - button_size - 15, 15, button_size, button_size]
+button_color = tuple(map(lambda x: x - 30, s.colors['screen']))
 
+clues = ['LMB - open', 'RMB - put/remove', 'flag']
             
 def choose_dificulty():
     new_call = True
@@ -19,10 +23,10 @@ def choose_dificulty():
     but_size, mess_size = 35, 50
     mess_font = pg.font.SysFont(s.font_name, mess_size)
     
-    BUTTON_WIDTH = WIDTH // 5
-    BUTTON_HEIGHT = HEIGHT // 6
-    OFFSET_Y = int(HEIGHT * (2 / 3))
-    DISTANCE = WIDTH // 10
+    BUTTON_WIDTH = s.width // 5
+    BUTTON_HEIGHT = s.height // 6
+    OFFSET_Y = int(s.height * (2 / 3))
+    DISTANCE = s.width // 10
     choose_msg = 'Choose difficulty:'
     message = mess_font.render(choose_msg, True , s.colors['text'])
     difs = ['easy', 'medium', 'hard']
@@ -31,8 +35,8 @@ def choose_dificulty():
     while True:
         mouse = pg.mouse.get_pos()
         if new_call:
-            s.screen.fill(SCREEN_COLOR)
-            s.screen.blit(message, ((s.width - len(choose_msg) * mess_size // 2) // 2, HEIGHT // 3))
+            s.screen.fill(s.colors['screen'])
+            s.screen.blit(message, ((s.width - len(choose_msg) * mess_size // 2) // 2, s.height // 3))
             dist = DISTANCE
             for dif in difs:
                 cord = (dist, OFFSET_Y, BUTTON_WIDTH, BUTTON_HEIGHT)
@@ -72,8 +76,11 @@ def main_menu():
     picture = pg.image.load('assets/main.jpg').convert_alpha()
     mini_picture = pg.transform.scale(picture, picture_cords[2:])
     play_button_color = tuple(map(lambda x: x - 25, s.colors['screen']))
+    
+    pg.mixer.music.load('music/meatball.wav')
+    pg.mixer.music.play(-1)
     while True:
-        s.screen.fill(SCREEN_COLOR)
+        s.screen.fill(s.colors['screen'])
         mouse = pg.mouse.get_pos()
         s.screen.blit(msg_font.render(msg, True, s.colors['text']), msg_cords)
         s.screen.blit(mini_picture, picture_cords[:2])
@@ -101,8 +108,8 @@ def draw_lines(screen, size, cell_size):
     for i in range(0, size + 1):
         x = left + i * (cell_size + s.line_width)
         y = top + i * (cell_size + s.line_width)
-        pg.draw.line(screen, LINE_COLOR, (x, top), (x, top + height), LINE_WIDTH)
-        pg.draw.line(screen, LINE_COLOR, (left, y), (left + width, y), LINE_WIDTH)
+        pg.draw.line(screen, s.colors['line'], (x, top), (x, top + height), s.line_width)
+        pg.draw.line(screen, s.colors['line'], (left, y), (left + width, y), s.line_width)
 
 
 def stop_game(game, type=0):
@@ -111,20 +118,22 @@ def stop_game(game, type=0):
         0: {'message': 'You lose!', 'main_pic': 'cat.png', 'button_set': ('home.png', 'retry.png')},
         1: {'message': 'You win!', 'main_pic': 'omniman.jpg', 'button_set': ('home.png', 'next.png')},
         2: {'message': 'Game paused!', 'main_pic': '', 'button_set': ('home.png', 'retry.png', 'continue.png')}}
-    mess_font = pg.font.SysFont(s.font_name, s.font_size)
-    msg = mess_font.render(end[type]['message'], True, s.colors['text'])
+    
     record = f'Your record: {game.record}'
+    mess_font = pg.font.SysFont(s.font_name, s.font_size)
     record_msg = mess_font.render(record, True, s.colors['text'])
+    
     x, y, a, b = s.width // 4, s.height // 4, s.width // 2, s.height // 1.8
     rect_cords = [x, y, a, b]
     button_1_cords = [x + a // 10, y + int(b * 23/30), a // 5, b // 5]
     button_2_cords = [x + int(a * 7/10), button_1_cords[1], a // 5, b // 5]
     main_pic_cords = [x + a // 4, y + b // 4, a // 2, b // 2]
-    rect_color = s.colors['screen']
-    button_color = tuple(map(lambda x: x - 30, s.colors['screen']))
     center_x = x + a // 2
     center_y = y + b // 2
     record_rect = record_msg.get_rect(center=(center_x, center_y - s.font_size * 3.5))
+    
+    rect_color = s.colors['screen']
+    button_color = tuple(map(lambda x: x - 30, s.colors['screen']))
     
     pref = 'assets/'
     home_pic = pg.transform.scale(pg.image.load(pref + end[type]['button_set'][0]).convert_alpha(), button_1_cords[2:])
@@ -183,7 +192,7 @@ def stop_game(game, type=0):
                     pg.mixer.Sound('sounds/button.wav').play()
                     game.reset()
                     return
-                elif type == 2 and is_button_pressed(mouse, button_3_cords):
+                elif type == 2 and (is_button_pressed(mouse, button_3_cords) or is_button_pressed(mouse, stop_button_cords)):
                     pg.mixer.Sound('sounds/button.wav').play()
                     game.is_new = True
                     return
@@ -194,31 +203,16 @@ def main():
     pg.font.init()
     pg.mixer.init()
     
-    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    screen = pg.display.set_mode((s.width, s.height))
     s.screen = screen
     pg.display.set_caption('Minesweeper')
     icon = pg.image.load('assets/icon.png')
     pg.display.set_icon(icon)
-
+    
     dificulty = main_menu()
     game = Game(dificulty)
     
-    clue_font = pg.font.SysFont(s.font_name, s.mini_font_size)
-    info_font = pg.font.SysFont(s.font_name, s.font_size)
-    record_font = pg.font.SysFont(s.font_name, s.font_size)
     
-    clue_cords = [s.window[0] // 12, s.window[1] * 1.5]
-    bomb_cords = (clue_cords[0], s.height // 2.25)
-    flag_cords = (clue_cords[0], bomb_cords[1] + s.big_font_size)
-    cur_record_cords = [s.window[0] * 1.20, s.window[1] // 4]
-    best_record_cords = [s.window[0] + s.window[2] - cur_record_cords[0], cur_record_cords[1]]
-    flag_amount_cords = [flag_cords[0] + s.big_font_size, flag_cords[1]]
-    
-    button_size = s.window[2] // 8
-    stop_button_cords = [s.width - button_size - 15, 15, button_size, button_size]
-    button_color = tuple(map(lambda x: x - 30, s.colors['screen']))
-    
-    clues = ['LMB - open', 'RMB - put/remove', 'flag']
     bomb = pg.image.load('assets/bomb.png').convert_alpha()
     flag = pg.image.load('assets/flag.png').convert_alpha()
     pause = pg.image.load('assets/pause.png').convert_alpha()
@@ -227,24 +221,30 @@ def main():
     mini_flag = pg.transform.scale(flag, (s.big_font_size, s.big_font_size))
     mini_pause = pg.transform.scale(pause, (button_size, button_size))
     
+    clue_font = pg.font.SysFont(s.font_name, s.mini_font_size)
+    info_font = pg.font.SysFont(s.font_name, s.font_size)
+    record_font = pg.font.SysFont(s.font_name, s.font_size)
+    
     available_side = min(s.window[2], s.window[3])
     s.cell_size = max(1, (available_side - s.line_width * (game.game_field.size - 1)) // game.game_field.size)
     while game.running:
-        
-
         cur = f'Record - {game.record}'
         pg.draw.rect(screen, s.colors['screen'], [*flag_amount_cords, s.big_font_size * 1.75, s.big_font_size])
         s.screen.blit(info_font.render(f' - {game.flags}', True, s.colors['text']), flag_amount_cords)
         if game.is_new:
+            pg.mixer.music.pause()
             best = game.get_best_result()
             best_msg = f'Best - {best}'
             
-            screen.fill(SCREEN_COLOR)
-            pg.draw.rect(screen, FRAME_COLOR, s.window)
-            pg.draw.rect(screen, button_color, stop_button_cords, border_radius=10)
-            pg.draw.rect(screen, tuple(map(lambda x: x - 30, button_color)), stop_button_cords, width=3, border_radius=10)
+            screen.fill(s.colors['screen'])
+            pg.draw.rect(screen, s.colors['frame'], s.window)
             draw_lines(screen, game.game_field.size, s.cell_size)
-            s.screen.blit(mini_pause, stop_button_cords[:2])
+            
+            draw_picture_button(
+                s.screen, button_color, stop_button_cords,
+                border_radius=10, picture=mini_pause, width=3,
+                line_color=tuple(map(lambda x: x - 30, button_color))
+                            )
             s.screen.blit(clue_font.render(clues[0], True, s.colors['text']), clue_cords)
             s.screen.blit(clue_font.render(clues[1], True, s.colors['text']), [clue_cords[0], clue_cords[1] + s.mini_font_size * 2])
             s.screen.blit(clue_font.render(clues[2], True, s.colors['text']), [clue_cords[0], clue_cords[1] + s.mini_font_size * 4])
@@ -278,6 +278,7 @@ def main():
                             game.mine_generate(cell_x, cell_y)
                         elif not game.game_field.field[cell_x][cell_y].has_flag:
                             new_cell = game.open_cell(cell_x, cell_y)
+                            pg.mixer.Sound('sounds/button.wav').play()
                             if new_cell.value == -1:
                                 game.show_mines()
                                 stop_game(game)
@@ -286,16 +287,13 @@ def main():
                                 if game.record > best:
                                     game.change_result()
                                 stop_game(game, 1)
-                            else:
-                                pg.mixer.Sound('sounds/button.wav').play()
                     elif event.button == 3:
                         if not game.game_field.field[cell_x][cell_y].has_flag:
                             if game.flags > 0:
                                 game.put_flag(cell_x, cell_y)
                         else:
                             game.remove_flag(cell_x, cell_y)
-                elif (stop_button_cords[0] <= mouse[0] <= stop_button_cords[0] + stop_button_cords[2] and
-                      stop_button_cords[1] <= mouse[1] <= stop_button_cords[1] + stop_button_cords[3]):
+                elif is_button_pressed(mouse, stop_button_cords):
                     pg.mixer.Sound('sounds/pause.wav').play()
                     stop_game(game, 2)
         pg.display.update()
